74c812ca84fd43b48b1cd5c90fb9c4b8d10e26dc
lingjin <464480515@qq.com>
2022-05-25 17:07:15 +0800

    git init project

---
+++ src/main/java/com/lingjin/surge/SurgeApplication.java
@@ -0,0 +1,13 @@
+package com.lingjin.surge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lingjin/surge/SurgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingjin.surge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
