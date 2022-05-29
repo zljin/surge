9681feaca4d415f34c062fdeae74f4eae043e3ea
lingjin <464480515@qq.com>
2022-05-29 12:03:54 +0800

    feat: add luna redis lock

--- src/main/java/com/example/infrastructure/schedule/ZhiHuSpiderUserJob.java
+++ src/main/java/com/example/infrastructure/schedule/ZhiHuSpiderUserJob.java
@@ -1,11 +1,16 @@
 package com.example.infrastructure.schedule;
 
+import com.example.infrastructure.util.RedisLock;
 import com.example.service.ZhiHuSpiderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Date;
 
 /**
  * @author leonard
@@ -22,14 +27,25 @@ public class ZhiHuSpiderUserJob {
     @Value("${zhihu.spiderjob.enabled:false}")
     private boolean enabled;
 
+    @Resource(name = "redisCommonTemplate")
+    private RedisTemplate<String, Object> redisTemplate;
+
 
     //5分钟执行一次
     @Scheduled(cron = "0 0/2 * * * *")
     public void cron2Db() throws InterruptedException {
-        if (!enabled) {
-            return;
+        try (RedisLock redisLock = new RedisLock(redisTemplate, "zhihu:url:cron:lock", 30)) {
+            if (redisLock.getLock()) {
+                if (!enabled) {
+                    return;
+                }
+                log.info("cronDB ...");
+                zhiHuSpiderService.consumerUserUrl();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        zhiHuSpiderService.consumerUserUrl();
+
     }
 
 }
